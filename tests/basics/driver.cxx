318ab14924e3155cdddd283a2a250ee887aba165
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2024-01-02 02:03:59 +0100

    bdep new -t lib mylib

---
+++ tests/basics/driver.cxx
@@ -0,0 +1,35 @@
+#include <sstream>
+#include <stdexcept>
+
+#include <mylib/version.hxx>
+#include <mylib/mylib.hxx>
+
+#undef NDEBUG
+#include <cassert>
+
+int main ()
+{
+  using namespace std;
+  using namespace mylib;
+
+  // Basics.
+  //
+  {
+    ostringstream o;
+    say_hello (o, "World");
+    assert (o.str () == "Hello, World!\n");
+  }
+
+  // Empty name.
+  //
+  try
+  {
+    ostringstream o;
+    say_hello (o, "");
+    assert (false);
+  }
+  catch (const invalid_argument& e)
+  {
+    assert (e.what () == string ("empty name"));
+  }
+}
